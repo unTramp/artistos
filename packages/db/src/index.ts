f20f6b9f549d39a4ee69bb89c04602c232048425
Andrey Dorofeev <dorfandy@gmail.com>
2026-09-16 15:50:32 +0400

    feat: export outbox consumer

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,4 +2,5 @@ export * from "./schema";
 export * from "./runtime";
 export * from "./artist-workspace-writer";
 export * from "./artist-scope";
+export * from "./outbox-dispatcher";
 export * from "./job-queue";
