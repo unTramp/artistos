830deb0e26aa52d443ae04497549a334932a63b7
Andrey Dorofeev <dorfandy@gmail.com>
2026-09-19 08:50:03 +0400

    refactor: keep locale switcher client-safe

--- apps/web/lib/i18n-server.ts
+++ apps/web/lib/i18n-server.ts
@@ -1,7 +1,5 @@
 import { cookies } from "next/headers";
-import { normalizeLocale, type Locale } from "./i18n";
-
-export const localeCookieName = "artist-os-locale";
+import { localeCookieName, normalizeLocale, type Locale } from "./i18n";
 
 export async function getServerLocale(): Promise<Locale> {
   const store = await cookies();
